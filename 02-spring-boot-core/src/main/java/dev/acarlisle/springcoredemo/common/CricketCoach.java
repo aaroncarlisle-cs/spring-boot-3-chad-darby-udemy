e1caeb6a3aa59d7d6b6a761ee0ff1c1f6dedf0b9
Aaron Carlisle <aaroncarlisle.cs@gmail.com>
2023-07-20 13:31:10 -0600

    Change CricketCoach to use prototype scope

--- 02-spring-boot-core/src/main/java/dev/acarlisle/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-core/src/main/java/dev/acarlisle/springcoredemo/common/CricketCoach.java
@@ -1,9 +1,12 @@
 package dev.acarlisle.springcoredemo.common;
 
 import dev.acarlisle.springcoredemo.common.Coach;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
 
     @Override
