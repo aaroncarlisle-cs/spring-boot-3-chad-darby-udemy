09c2566439ae62d55e25c16cdc7fe2b06b421a4c
Aaron Carlisle <aaroncarlisle.cs@gmail.com>
2023-07-20 12:15:42 -0600

    Create method getDailyWorkout for dailyworkout endpoint

--- 02-spring-boot-core/src/main/java/dev/acarlisle/springcoredemo/DemoController.java
+++ 02-spring-boot-core/src/main/java/dev/acarlisle/springcoredemo/DemoController.java
@@ -1,13 +1,19 @@
 package dev.acarlisle.springcoredemo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
-        private Coach myCoach;
+    private Coach myCoach;
 
     public DemoController(Coach myCoach) {
         this.myCoach = myCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
     }
 }
