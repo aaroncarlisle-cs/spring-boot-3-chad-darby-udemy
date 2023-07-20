f627b2fcd062798dd6d7806365908bcd659085ce
Aaron Carlisle <aaroncarlisle.cs@gmail.com>
2023-07-20 13:41:06 -0600

    Add lifecycle methods for CricketCoach

--- 02-spring-boot-core/src/main/java/dev/acarlisle/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-core/src/main/java/dev/acarlisle/springcoredemo/common/CricketCoach.java
@@ -1,5 +1,7 @@
 package dev.acarlisle.springcoredemo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,7 +9,20 @@ public class CricketCoach implements Coach {
 
     @Override
     public String getDailyWorkout() {
+
         return "Practice fast bowling for 15 minutes!";
+    }
+
+    @PostConstruct
+    public void doMyStartupStuff() {
+
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doMyCleanupStuff() {
+
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
     }
 
 }
