2de670ed5801e97de0fef57e132b036346466b92
Aaron Carlisle <aaroncarlisle.cs@gmail.com>
2023-07-20 12:13:41 -0600

    Create constructor for DemoController

--- 02-spring-boot-core/src/main/java/dev/acarlisle/springcoredemo/DemoController.java
+++ 02-spring-boot-core/src/main/java/dev/acarlisle/springcoredemo/DemoController.java
@@ -5,4 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
+        private Coach myCoach;
+
+    public DemoController(Coach myCoach) {
+        this.myCoach = myCoach;
+    }
 }
