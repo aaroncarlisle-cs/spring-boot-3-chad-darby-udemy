62341c8063b581c9293697084d3b991c2a12cdb3
Aaron Carlisle <aaroncarlisle.cs@gmail.com>
2023-08-25 12:40:21 -0600

    Create Student REST controller

---
+++ 04-spring-boot-rest-crud/src/main/java/dev/acarlisle/springbootrestcrud/rest/StudentRestController.java
@@ -0,0 +1,12 @@
+package dev.acarlisle.springbootrestcrud.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    // define an endpoint for /students - return a list of students
+
+}
