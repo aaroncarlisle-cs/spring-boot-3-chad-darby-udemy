b24e9714da66b2c7616d998e8195cc24e2916336
Aaron Carlisle <aaroncarlisle.cs@gmail.com>
2023-08-25 12:10:23 -0600

    Create DemoRestController

---
+++ 04-spring-boot-rest-crud/src/main/java/dev/acarlisle/springbootrestcrud/rest/DemoRestController.java
@@ -0,0 +1,11 @@
+package dev.acarlisle.springbootrestcrud.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class DemoRestController {
+
+    // add code for the "/hello" endpoint
+}
