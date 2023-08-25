33113d6d01d85e2acb12418e4b47d3b99977bf54
Aaron Carlisle <aaroncarlisle.cs@gmail.com>
2023-08-25 12:42:50 -0600

    Create endpoint for /students

--- 04-spring-boot-rest-crud/src/main/java/dev/acarlisle/springbootrestcrud/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/src/main/java/dev/acarlisle/springbootrestcrud/rest/StudentRestController.java
@@ -1,7 +1,12 @@
 package dev.acarlisle.springbootrestcrud.rest;
 
+import dev.acarlisle.springbootrestcrud.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -9,4 +14,15 @@ public class StudentRestController {
 
     // define an endpoint for /students - return a list of students
 
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
+        List<Student> students = new ArrayList<>();
+
+        students.add(new Student("Poornima", "Patel"));
+        students.add(new Student("Mario", "Rossi"));
+        students.add(new Student("Mary", "Smith"));
+
+        return students;
+    }
 }
