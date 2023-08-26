7f3b0346f2fb1b2224a2ef9fb5c3860eef52622f
Aaron Carlisle <aaroncarlisle.cs@gmail.com>
2023-08-26 12:54:53 -0600

    Define table fields

--- 04-spring-boot-rest-crud-demo-project/src/main/java/dev/acarlisle/restcruddemo/entity/Employee.java
+++ 04-spring-boot-rest-crud-demo-project/src/main/java/dev/acarlisle/restcruddemo/entity/Employee.java
@@ -1,4 +1,28 @@
 package dev.acarlisle.restcruddemo.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="employee")
 public class Employee {
+
+    // define fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+    // define constructors
+
+    // define getter/setter
+
+    // define toString
 }
