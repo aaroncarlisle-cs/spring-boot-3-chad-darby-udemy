d6e42ff8335cd1ed61f4b5294dac167460ad6ec4
Aaron Carlisle <aaroncarlisle.cs@gmail.com>
2023-08-26 16:45:13 -0600

    Change repository path to members

--- 04-spring-boot-rest-crud-demo-project-jpa-with-spring-data/src/main/java/dev/acarlisle/restcruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud-demo-project-jpa-with-spring-data/src/main/java/dev/acarlisle/restcruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package dev.acarlisle.restcruddemo.dao;
 
 import dev.acarlisle.restcruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="/members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it... no need to write any code!
