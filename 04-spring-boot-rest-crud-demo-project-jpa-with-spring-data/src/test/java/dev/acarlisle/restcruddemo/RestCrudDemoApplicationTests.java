d7275835e3e33e25c2f31a3cc671a0ceacf419c0
Aaron Carlisle <aaroncarlisle.cs@gmail.com>
2023-08-26 15:01:04 -0600

    Initial project commit

---
+++ 04-spring-boot-rest-crud-demo-project-jpa-with-spring-data/src/test/java/dev/acarlisle/restcruddemo/RestCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.acarlisle.restcruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
